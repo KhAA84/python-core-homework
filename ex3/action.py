e18f6c8c77bc9c3b562da4ea9d12267f955b72c3
KhAA84 <khatlamadgian.a@gmail.com>
2022-02-06 15:07:59 +0300

    homework completed

--- ex3/action.py
+++ ex3/action.py
@@ -5,22 +5,36 @@ class BaseAction:
     def __repr__(self):
         return self.name
 
+    def __eq__(self, other):
+        return self.name == other.name    
+
+    def __gt__(self, other):
+        return NotImplemented
+
+    def __hash__(self):
+        return self.name.__hash__()   
 
 class NothingAction(BaseAction):
     def __init__(self):
         super().__init__('Nothing')
 
-
 class RockAction(BaseAction):
     def __init__(self):
         super().__init__('Rock')
 
+    def __gt__(self, other):
+        return isinstance(other, ScissorsAction)
 
 class PaperAction(BaseAction):
     def __init__(self):
         super().__init__('Paper')
 
+    def __gt__(self, other):
+        return isinstance(other, RockAction)
 
 class ScissorsAction(BaseAction):
     def __init__(self):
         super().__init__('Scissors')
+
+    def __gt__(self, other):
+        return isinstance(other, PaperAction)
